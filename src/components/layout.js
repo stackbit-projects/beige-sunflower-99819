59aa0803cfc09eadc072f2b9dce6d8d5a336b6ad
Szymon Wolszczak <drail901@gmail.com>
2021-02-02 18:26:08 +0000

    layout.js: updated Link 3 in List

--- src/components/layout.js
+++ src/components/layout.js
@@ -33,7 +33,7 @@ const Layout = props => {
                 <Link to={`/about`}>About</Link>
               </li>
               <li className="nav-elements" role="menuitem">
-                <Link to={`/elements`}>Elements</Link>
+                <Link to={`/elements`}>Elements33</Link>
               </li>
             </ul>
           </nav>
